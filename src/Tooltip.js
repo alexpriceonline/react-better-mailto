8f4f42773e2f527abb63458a90cb164625591ad3
gurjit03 <gurjitmehta@gmail.com>
2018-01-30 02:01:13 +0530

    added support for styles through props

--- src/Tooltip.js
+++ src/Tooltip.js
@@ -18,19 +18,25 @@ const openEmailClient = email => {
   document.location = `mailto:${email}`;
 };
 
-const Tooltip = ({ emailAddress, className }) => (
-  <div className={className + '__tooltip'} style={toolTipStyles}>
-    <div className={className + '__tooltip-inner-wrapper'} style={toolTipInnerWrapper}>
-      <div className={className + '__tooltip-head'} style={toolTipHead}>
-        {emailAddress}
+const Tooltip = props => (
+  <div className={props.className + '__tooltip'} style={toolTipStyles || props.toolTipStyles}>
+    <div
+      className={props.className + '__tooltip-inner-wrapper'}
+      style={toolTipInnerWrapper || props.toolTipInnerWrapper}
+    >
+      <div className={props.className + '__tooltip-head'} style={toolTipHead || props.toolTipHead}>
+        {props.emailAddress}
       </div>
-      <div className={className + '__tooltip-body'}>
-        <CopyToClipboard text={emailAddress} onCopy={() => alert('copied.')}>
+      <div className={props.className + '__tooltip-body'}>
+        <CopyToClipboard text={props.emailAddress} onCopy={() => alert('copied.')}>
           <button
-            className={className + '__tooltip--copy-email-address __tooltip-body-btn'}
-            style={toolTipBodyBtnStyles}
+            className={props.className + '__tooltip--copy-email-address __tooltip-body-btn'}
+            style={toolTipBodyBtnStyles || props.toolTipBtnStyles}
           >
-            <span className={className + '__tooltip-body-btn--text'} style={toolTipBtnText}>
+            <span
+              className={props.className + '__tooltip-body-btn--text'}
+              style={toolTipBtnText || props.toolTipBtnText}
+            >
               Copy Email Address To ClipBoard
             </span>
           </button>
@@ -38,22 +44,32 @@ const Tooltip = ({ emailAddress, className }) => (
 
         <a
           href={`mailto:${emailAddress}`}
-          className={className + '__tooltip--compose-default-email __tooltip-compose-default-email'}
-          style={toolTipBodyBtnStyles}
+          className={props.className + '__tooltip--compose-default-email __tooltip-compose-default-email'}
+          style={toolTipBodyBtnStyles || props.toolTipBodyBtnStyles}
         >
-          <span className={className + '__tooltip-body-btn--text'} style={toolTipAnchorText}>
+          <span
+            className={props.className + '__tooltip-body-btn--text'}
+            style={toolTipAnchorText || props.toolTipAnchorText}
+          >
             Compose email in default client
           </span>
         </a>
       </div>
     </div>
-    <span className={className + '__tooltip-arrow'} style={afterStyles} />
+    <span className={props.className + '__tooltip-arrow'} style={afterStyles || props.afterStyles} />
   </div>
 );
 
 Tooltip.propTypes = {
   emailAddress: t.string.isRequired,
-  className: t.string
+  className: t.string,
+  afterStyles: t.object,
+  toolTipAnchorText: t.object,
+  toolTipBodyBtnStyles: t.object,
+  toolTipBtnText: t.object,
+  toolTipBtnStyles: t.object,
+  toolTipInnerWrapper: t.object,
+  toolTipStyles: t.object
 };
 
 export default Tooltip;
